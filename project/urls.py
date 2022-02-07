48b6df7bfa17d64935b9d96d0de82443cd54da55
Rob866 <juanrob_10@hotmail.com>
2022-02-06 19:47:55 -0700

    Change the admin to a different URL

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('office/', admin.site.urls),
 ]
 
 # Enable the debug toolbar only in DEBUG mode.
