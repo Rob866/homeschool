f10b9cb64f34e15eb31f7bc604742934d3301841
Rob866 <juanrob_10@hotmail.com>
2022-02-06 18:25:04 -0700

    Use django-environ for configuration handling and add the django-debug-toolbar

--- project/settings.py
+++ project/settings.py
@@ -9,22 +9,29 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 import django_heroku
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(DEBUG=(bool, False),DEBUG_TOOLBAR=(bool,False))
+env_file = os.path.join(BASE_DIR,'.env')
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-01-!_awi5%d5#9h9_gmu_v-y4bmrfbp-zehh%(ua0y30-f_b$j'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
+DEBUG = env('DEBUG')
+DEBUG_TOOLBAR= env('DEBUG_TOOLBAR')
 ALLOWED_HOSTS = []
 
 # Application definition
@@ -47,6 +54,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+if DEBUG and DEBUG_TOOLBAR:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+    INTERNAL_IPS = ["127.0.0.1"]
+
 
 ROOT_URLCONF = 'project.urls'
 
