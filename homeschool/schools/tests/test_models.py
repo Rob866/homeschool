ddf2c8816a8c74444eee3bc1a09259da78c83c4b
Rob866 <juanrob_10@hotmail.com>
2022-02-12 14:39:42 -0700

    Use django-test-plus.

--- homeschool/schools/tests/test_models.py
+++ homeschool/schools/tests/test_models.py
@@ -1,19 +1,10 @@
-from django.test import TestCase
-
 from homeschool.schools.tests.factories import SchoolFactory
-from homeschool.users.tests.factories import UserFactory
+from homeschool.test import TestCase
 
 
 class TestSchool(TestCase):
-    def test_factory(self):
-        """A school is created."""
-        school = SchoolFactory()
-
-        self.assertIsNotNone(school)
-
     def test_school_has_admin(self):
         """A school has an administrator."""
-        user = UserFactory()
+        user = self.make_user()
         school = SchoolFactory(admin=user)
-
         self.assertEqual(school.admin, user)
